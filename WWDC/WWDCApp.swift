bbde9d38907af66a8ebc0e85f2c6ed6bce3998af
caijunzhi <chickenbilibili@outlook.com>
2023-11-20 15:59:59 +0800

    initial commit

---
+++ WWDC/WWDCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCApp.swift
+//  WWDC
+//
+//  Created by 蔡俊志 on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct WWDCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
